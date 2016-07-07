e51a69a722009ceabd345c7fea20563f8e97ed2d
Apple <apple@DavidHongdeiMac.local>
2016-07-07 15:31:27 +0800

    “初始化”

---
+++ SHWeather/SHWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SHWeather
+//
+//  Created by Apple on 16/7/7.
+//  Copyright © 2016年 Shenzhen iDooFly Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SHWeather/SHWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SHWeather
+//
+//  Created by Apple on 16/7/7.
+//  Copyright © 2016年 Shenzhen iDooFly Technology Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
